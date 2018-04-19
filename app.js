28cb0005c9acf471b4a085d21c063f23be16f6b4
richardsunny2015 <richardsunny2015@gmail.com>
2018-04-19 11:41:44 -0400

    we rendered

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const app = express();
 const volleyball = require('volleyball')
- 
+const nunjucks = require('nunjucks');
+
+let locals = {
+  title: 'An Example',
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'},
+    {name: 'Sam'}
+  ]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+nunjucks.configure('views', {noCache: true, autoescape: true, express: app})
+
+// nunjucks.render('index.html', locals, function(err, output) {
+//   console.log(output);
+// })
+
 app.use(volleyball)
+
 
 // app.use('/', (req, res, next) => {
 //     console.log(req.method + ' /');
@@ -17,7 +38,9 @@ app.use(volleyball)
 // })
 
 app.get('/', (req, res) => {
-    res.send('Hello welcome to Richard\'s Nice Things Emporium!');
+    // res.send('Hello welcome to Richard\'s Nice Things Emporium!');
+    res.render('index', locals)
+    // res.render('views/index.html');
 })
 
 app.get('/news', (req, res) => {
@@ -26,4 +49,3 @@ app.get('/news', (req, res) => {
 
 
 app.listen(3000, () => console.log('Hello World!'));
-
